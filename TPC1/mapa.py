b3c88d0b40e9e57315a52287ab76292a7ea01222
Hugo Dias <116220916+hugodiasUM@users.noreply.github.com>
2024-02-06 15:59:12 +0000

    lj

--- TPC1/mapa.py
+++ TPC1/mapa.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 html = '''
 <!DOCTYPE html>
@@ -10,14 +11,35 @@ html = '''
 <body>
 '''
 
+template = html
+
 file = open("mapa.json", "r", encoding=("utf-8")).read()
+os.mkdir("html")
 
 content = json.loads(file)
 
 html += "<ul>"
 
+listaCidades = []
 for elem in content["cidades"]:
-    html += f"<li>{elem["nome"]}</li>"
+    listaCidades.append(elem["nome"])
+
+    templateCidade = template
+    templateCidade += f"<h1{elem["nome"]}</h1>"
+    templateCidade += f"<h3{elem["distrito"]}</h3>"
+    templateCidade += f"<p><b>População:</b> {elem["população"]}</p>"
+    templateCidade += f"<p><b>Descrição:</b> {elem["descrição"]}</p>"
+    templateCidade += f'<h6><a href="../mapa.html">Voltar</a></h6>'
+    
+    templateCidade += "</body>"
+    templateCidade += "</html>"
+
+    fileCidade = open(f"html/{elem["nome"]}.html", "w", encoding="utf-8")
+    fileCidade.write(templateCidade)
+    fileCidade.close()
+
+for elem in sorted(listaCidades):
+    html += f'<li><a href="html/{elem}.html">{elem}</a></li>'
 
 html += "</ul>"
 
